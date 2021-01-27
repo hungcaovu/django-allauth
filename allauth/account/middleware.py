09cc54e48f4480cbd704e575cf90bbb90350c91c
Cao Hung <hung.caovu@gmail.com>
2021-01-28 01:03:45 +0700

    add midle ware

---
+++ allauth/account/middleware.py
@@ -0,0 +1,18 @@
+from .models import UserProfile
+
+class AccountProfileMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        setattr(request, 'profile', request.user.profile)
+        
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
